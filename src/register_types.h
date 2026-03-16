79d0906a6c2d6b32d4fcfe9166b47fee2a54f245
abdelhakim mahha <82586828+haki-m@users.noreply.github.com>
2026-03-16 22:06:59 +0000

    Add files via upload

---
+++ src/register_types.h
@@ -0,0 +1,11 @@
+#ifndef MP4_REGISTER_TYPES_H
+#define MP4_REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_mp4_module(ModuleInitializationLevel p_level);
+void uninitialize_mp4_module(ModuleInitializationLevel p_level);
+
+#endif
